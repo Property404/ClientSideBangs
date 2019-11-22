9f3cc808f33caf3960ade33dbeb07092677d41b2
Property404 <martinez.dagan@gmail.com>
2019-11-22 11:58:20 -0600

    Sort bangs list

--- common.js
+++ common.js
@@ -1,15 +1,15 @@
 const DEFAULT_BANG_DICTIONARY = 
 	{
-		"w":"https://wikipedia.org/wiki/Search?search=",
-		"g":"https://www.google.com/search?hl=en&q=",
-		"bing":"https://www.bing.com/search?q=",
-		"b":"https://www.bing.com/search?q=",
-		"bv":"https://www.bing.com/videos?q=",
-		"yt":"https://www.youtube.com/results?search_query=",
-		"r":"https://www.reddit.com/search?q=",
-		"gh":"https://github.com/search?q=",
 		"a":"https://www.amazon.com/s/?field-keywords=",
-		"tw":"https://twitter.com/search?q=",
-		"spotify":"https://open.spotify.com/search/",
+		"b":"https://www.bing.com/search?q=",
+		"bing":"https://www.bing.com/search?q=",
+		"bv":"https://www.bing.com/videos?q=",
+		"g":"https://www.google.com/search?hl=en&q=",
+		"gh":"https://github.com/search?q=",
 		"mdn":"https://developer.mozilla.org/en-US/search?q="
+		"r":"https://www.reddit.com/search?q=",
+		"spotify":"https://open.spotify.com/search/",
+		"tw":"https://twitter.com/search?q=",
+		"w":"https://wikipedia.org/wiki/Search?search=",
+		"yt":"https://www.youtube.com/results?search_query=",
 	}
