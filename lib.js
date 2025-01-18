46db0f8dddcaba512a1369ac385eac336d6a0423
Property404 <git@dagans.dev>
2025-01-18 12:26:48 -0600

    Get rid of some default bangs

--- lib.js
+++ lib.js
@@ -2,17 +2,12 @@
 
 const DEFAULT_BANG_DICTIONARY = 
 	{
-		"a":"https://www.amazon.com/s/?field-keywords=",
-		"b":"https://www.bing.com/search?q=",
-		"bing":"https://www.bing.com/search?q=",
-		"bv":"https://www.bing.com/videos?q=",
         "d": "https://dagans.dev/search?q=",
 		"g":"https://www.google.com/search?hl=en&q=",
 		"gh":"https://github.com/search?q=",
-		"mdn":"https://developer.mozilla.org/en-US/search?q=",
 		"r":"https://www.reddit.com/search?q=",
 		"spotify":"https://open.spotify.com/search/",
-		"w":"https://wikipedia.org/wiki/Search?search=",
+		"w":"https://wikipedia.org/wiki/Special:Search?search=",
 		"yt":"https://www.youtube.com/results?search_query=",
 	};
 
@@ -86,7 +81,7 @@ function listener(req)
     }
 }
 
-if (require.main === module) {
+if (require?.main === module) {
     const assert = require('assert');
 
     assert(extractBang("!g hello") == "g");
