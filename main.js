ed008c75b7fac06a2866a38d07f516855f105951
Property404 <git@dagans.dev>
2025-01-18 12:31:40 -0600

    Only activate on google searches, not any google URL

--- main.js
+++ main.js
@@ -5,19 +5,17 @@ browser.webRequest.onBeforeRequest.addListener(
 	{
         urls: [
             "*://duckduckgo.com/*",
-            "*://www.google.com/*"
+            "*://www.google.com/search*"
         ]
     }
 );
 
 // Fetch settings after manual addon reloading.
 browser.storage.sync.get('sets').then(function (result) {
-    console.log("CSB: sets: " + result.sets);
 	bang_dictionary = result.sets ?? DEFAULT_BANG_DICTIONARY;
 });
 
 browser.storage.onChanged.addListener(function (changes, where){
-    console.log("CSB: addList");
 	if (changes.sets && where == 'sync') {
 		bang_dictionary = changes.sets.newValue ?? DEFAULT_BANG_DICTIONARY;
 	}
